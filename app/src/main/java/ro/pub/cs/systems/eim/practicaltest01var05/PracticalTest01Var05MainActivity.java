fdfc3f5254ab4da7c40931fb995d54e48467427f
DobreteVlad <dobrete.vlad@yahoo.com>
2022-04-11 08:31:34 +0300

    Ex B

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/PracticalTest01Var05MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/PracticalTest01Var05MainActivity.java
@@ -3,12 +3,85 @@ package ro.pub.cs.systems.eim.practicaltest01var05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PracticalTest01Var05MainActivity extends AppCompatActivity {
 
+    private Button buttonTL, buttonTR, buttonC, buttonBL, buttonBR, buttonNextActivity;
+    private EditText textField;
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    int totalClicks;
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            String aux;
+            switch(view.getId()) {
+                case R.id.practicB1:
+                    aux = textField.getText().toString() + "Top Left, ";
+                    textField.setText(aux);
+                    totalClicks++;
+                    break;
+                case R.id.practicB2:
+                    aux = textField.getText().toString() + "Top Right, ";
+                    textField.setText(aux);
+                    totalClicks++;
+                    break;
+                case R.id.practicB3:
+                    aux = textField.getText().toString() + "Center, ";
+                    textField.setText(aux);
+                    totalClicks++;
+                    break;
+                case R.id.practicB4:
+                    aux = textField.getText().toString() + "Bottom Left, ";
+                    textField.setText(aux);
+                    totalClicks++;
+                    break;
+                case R.id.practicB5:
+                    aux = textField.getText().toString() + "Bottom Right, ";
+                    textField.setText(aux);
+                    totalClicks++;
+                    break;
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var05_main);
+
+        totalClicks = 0;
+        buttonTL = (Button)findViewById(R.id.practicB1);
+        buttonTR = (Button)findViewById(R.id.practicB2);
+        buttonC = (Button)findViewById(R.id.practicB3);
+        buttonBL = (Button)findViewById(R.id.practicB4);
+        buttonBR = (Button)findViewById(R.id.practicB5);
+        buttonNextActivity = (Button)findViewById(R.id.practicBNextActivity);
+        textField = (EditText) findViewById(R.id.practicE1);
+
+        buttonTL.setOnClickListener(buttonClickListener);
+        buttonTR.setOnClickListener(buttonClickListener);
+        buttonC.setOnClickListener(buttonClickListener);
+        buttonBL.setOnClickListener(buttonClickListener);
+        buttonBR.setOnClickListener(buttonClickListener);
     }
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(Constants.NUMBER_OF_CLICKS, totalClicks);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState.containsKey(Constants.NUMBER_OF_CLICKS)) {
+            totalClicks = savedInstanceState.getInt(Constants.NUMBER_OF_CLICKS);
+        } else {
+            totalClicks = 0;
+        }
+        Toast.makeText(this, "The total clicks are " + totalClicks, Toast.LENGTH_LONG).show();
+    }
+
 }
